cd6ee9fcb4d2bcfcf0e2b487e82b78892c461ccf
Danny McClanahan <danieldmcclanahan@gmail.com>
2015-06-08 20:42:05 -0700

    removed unused file

--- install_coffee_if_not.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if ! hash coffee 2>/dev/null; then
-  echo "coffeescript is not installed on your system." 1>&2
-  echo "Please run \"npm install -g coffee-script\"." 1>&2
-  exit -1
-fi
